3ebcf4754e9fea3d72f45771010a6855ea23aabe
FOOLSIX <2580368016@qq.com>
2024-05-02 13:55:06 +0800

    0.0.4pre1

---
+++ src/main/java/com/foolsix/fancyenchantments/effect/Cumbersome.java
@@ -0,0 +1,22 @@
+package com.foolsix.fancyenchantments.effect;
+
+import com.foolsix.fancyenchantments.FancyEnchantments;
+import com.foolsix.fancyenchantments.util.ModConfig;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+
+import static com.foolsix.fancyenchantments.effect.EffectIDs.CUMBERSOME_UUID;
+
+public class Cumbersome extends MobEffect {
+    public static final ModConfig.CumbersomeOptions CONFIG = FancyEnchantments.getConfig().cumbersomeOptions;
+    public Cumbersome() {
+        super(MobEffectCategory.HARMFUL, 0x24B262);
+        super.addAttributeModifier(Attributes.ATTACK_SPEED,
+                                   CUMBERSOME_UUID.toString(),
+                                   -CONFIG.atkSpeedReducer / 2,
+                                   AttributeModifier.Operation.MULTIPLY_TOTAL);
+    }
+}
---
+++ src/main/java/com/foolsix/fancyenchantments/enchantment/Cumbersome.java
@@ -0,0 +1,37 @@
+package com.foolsix.fancyenchantments.enchantment;
+
+import com.foolsix.fancyenchantments.FancyEnchantments;
+import com.foolsix.fancyenchantments.enchantment.EssentiaEnch.TerraEnchantment;
+import com.foolsix.fancyenchantments.util.ModConfig;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+
+import static com.foolsix.fancyenchantments.effect.EffectReg.CUMBERSOME;
+
+public class Cumbersome extends TerraEnchantment {
+    private static final ModConfig.CumbersomeOptions CONFIG = FancyEnchantments.getConfig().cumbersomeOptions;
+    public Cumbersome() {
+        super(CONFIG, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+    }
+
+    @Override
+    public boolean isCurse() {
+        return true;
+    }
+
+    public void getCooldown(LivingAttackEvent e) {
+        if (e.getSource() == null || e.getSource().getEntity() == null) return;
+        if (e.getSource().getEntity() instanceof LivingEntity living) {
+            int level = EnchantmentHelper.getEnchantmentLevel(this, living);
+            if (level > 0 && Math.random() < CONFIG.probability) {
+                living.addEffect(new MobEffectInstance(CUMBERSOME.get(), CONFIG.duration * 20));
+            }
+        }
+    }
+}
