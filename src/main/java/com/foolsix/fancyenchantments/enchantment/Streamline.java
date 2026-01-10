bb94dc8579163993199162aaa1bfb32875a989da
FOOLSIX <2580368016@qq.com>
2026-01-10 19:12:37 +0800

    change:remove Remove the Streamline gravity-ignoring feature that caused issues

--- src/main/java/com/foolsix/fancyenchantments/enchantment/Streamline.java
+++ src/main/java/com/foolsix/fancyenchantments/enchantment/Streamline.java
@@ -10,7 +10,6 @@ import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
-import net.minecraftforge.event.entity.ProjectileImpactEvent;
 
 public class Streamline extends AerEnchantment {
     private static final ModConfig.StreamlineOptions CONFIG = FancyEnchantments.getConfig().streamlineOptions;
@@ -24,20 +23,10 @@ public class Streamline extends AerEnchantment {
         if (e.getEntity() instanceof AbstractArrow arrow && arrow.getOwner() instanceof LivingEntity shooter) {
             int level = EnchantmentHelper.getEnchantmentLevel(this, shooter);
             if (level > 0) {
-                arrow.setNoGravity(true);
                 arrow.addTag(TAG);
                 Vec3 direction = shooter.getViewVector(0.1f).scale(CONFIG.speedMultiplierPerLevel * level);
                 arrow.push(direction.x(), direction.y(), direction.z());
             }
-        }
-    }
-
-    public void impactInvulnerable(ProjectileImpactEvent e) {
-        var entity = e.getEntity();
-        var arrow = e.getProjectile();
-        if (entity == null || arrow == null) return;
-        if (arrow.getTags().contains(TAG)) {
-            arrow.setNoGravity(false);
         }
     }
 }
--- src/main/java/com/foolsix/fancyenchantments/events/EnchantmentEvents.java
+++ src/main/java/com/foolsix/fancyenchantments/events/EnchantmentEvents.java
@@ -121,7 +121,6 @@ public class EnchantmentEvents {
         }
         ((HeavyArrow) HEAVY_ARROW.get()).arrowImpact(e);
         ((AdvancedFlame) ADVANCED_FLAME.get()).arrowImpact(e);
-        ((Streamline) STREAMLINE.get()).impactInvulnerable(e);
     }
 
     @SubscribeEvent
